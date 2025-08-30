a0d418965b278501b4f5bec00bee67a9ba0ef634
ElIsuss <isaias.morinigo10@gmail.com>
2025-08-30 16:55:36 -0300

    Add files via upload

---
+++ Unidad 1/Punto 4.py
@@ -0,0 +1,8 @@
+print ("-----------Ejercicio 4-----------")
+
+radio = float(input("Dime el radio: "))
+area = 3.1415 * radio
+perimetro = 2 * 3.1415 * radio
+print("el aarea es de:", area)
+print("El perimetro es de:",perimetro)
+

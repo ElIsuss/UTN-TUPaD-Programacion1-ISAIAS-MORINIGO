6deb1cece8f7ff5e98eef9387b62ffbd56423bc7
ElIsuss <isaias.morinigo10@gmail.com>
2025-08-30 16:28:52 -0300

    Add files via upload

---
+++ Trabajos que propone la profe/Calcular propina de un restaurante.py
@@ -0,0 +1,10 @@
+total_pago = float(input("Total a pagar: "))
+propina_sugerida =  0.10
+pago_de_la_cuenta = total_pago * propina_sugerida
+print (pago_de_la_cuenta)
+print(pago_de_la_cuenta + total_pago)
+total_pago = float(input("Total a pagar: "))
+propina_sugerida = 0.15
+pago_de_la_cuenta = total_pago * propina_sugerida
+print(pago_de_la_cuenta)
+print (pago_de_la_cuenta + total_pago)

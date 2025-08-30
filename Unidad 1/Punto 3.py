cc39b6a2e53dd7d2e2047f5f471ed849df4e435f
ElIsuss <isaias.morinigo10@gmail.com>
2025-08-30 16:55:12 -0300

    Add files via upload

---
+++ Unidad 1/Punto 3.py
@@ -0,0 +1,7 @@
+print ("-----------Ejercicio 3-----------")
+
+nombre = str(input("Escribe tu nombre:"))
+apellido = str(input("Escribe tu apellido:"))
+edad = int(input("Dime tu edad:"))
+lugar_De_residencia = str(input("Dime donde vives:"))
+print( "soy", nombre, apellido,"tengo", edad, ", vivo en", lugar_De_residencia)

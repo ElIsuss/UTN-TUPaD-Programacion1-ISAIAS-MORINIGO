d759a6e7b1a890166fe4a220afcc4f3b84cfadb4
ElIsuss <isaias.morinigo10@gmail.com>
2025-08-30 16:28:30 -0300

    Add files via upload
    Primeros ejercicios complementarios

---
+++ Trabajos que propone la profe/Ejercicios complemetarios 1.py
@@ -0,0 +1,43 @@
+#punto1
+numero1 = 35
+#punto2
+numero2 = 18.9
+#punto3
+suma = numero1 + numero2
+#punto4
+resta = numero1 - numero2
+Multiplicacion = numero1 * numero2
+divicion = numero1 / numero2
+print (resta, Multiplicacion, divicion)
+#punto5 
+nombre = "isaias"
+#punto6 
+precio = 5000
+#punto7
+descuento = 0.50
+#punto8 
+precio_final = precio * descuento
+#punto9
+cadena = str("AGUANTE LA PULP DE NARANGA")
+#punto10
+longitud = len(cadena)
+#punto11
+precio2 = 39.9023
+precio2 = round (precio2)
+#punto12
+nombre2 = "isaias"
+apellido = "morinigo"
+nombre_completo = nombre2, " ", apellido
+#punto13
+edad = 18
+edad = edad + 5
+edad = edad - 10
+#punto14
+altura = 1.65
+altura = altura * 4
+altura = altura /3
+#punto15
+numbre = "ISAIAS MORINIGO"
+numbre = numbre.lower
+#punto16 
+numbre = numbre.capitalize()
